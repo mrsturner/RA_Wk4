b1ce0940c99e5def862f7e8f01719f7664d7ff32
mrsturner <allison.jns1@gmail.com>
2022-12-31 11:16:49 -0600

    alert revised

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function myFunction(name, height, country) {
-	alert(name + ' ' + height + ' ' + country);
+	alert('Hello, I am ' + name + '. I am  ' + height  + ' tall. ' + ' I live in the ' + country);
 }
 myFunction('Allison', '49in', 'US');
